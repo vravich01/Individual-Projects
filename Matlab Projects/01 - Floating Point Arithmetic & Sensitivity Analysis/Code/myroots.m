07346806d41f28c3e41f61b6f2d6cdf715d4170e
Vinayak Ravichandran <67216399+vravich01@users.noreply.github.com>
2021-08-27 14:32:57 -0400

    Update myroots.m

--- Matlab Projects/01 - Floating Point Arithmetic & Sensitivity Analysis/Code/myroots.m
+++ Matlab Projects/01 - Floating Point Arithmetic & Sensitivity Analysis/Code/myroots.m
@@ -1,6 +1,7 @@
+% This function is provided by Dr. Eric Hallman, from NCSU MA Department
+
 function [X_1, X_2] = myroots(a,b,c)
 format long
 X_1 = (-b + (sqrt(b.^2 - 4.*a.*c)))/(2.*a);
 X_2 = (-b - (sqrt(b.^2 - 4.*a.*c)))/(2.*a);
 end
-
